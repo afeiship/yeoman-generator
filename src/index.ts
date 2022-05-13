8858a5722d6618b712155dfa592bb6dc351d51e5
afeiship <1290657123@qq.com>
2022-05-14 07:11:35 +0800

    feat: add basic code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export default () => {
-  console.log('Hello World');
+import Generator from 'yeoman-generator';
+import globby from 'globby';
+import yoHelper from '@jswork/yeoman-generator-helper';
+
+export default class YeomanGenerator extends Generator {
+  protected ctx = yoHelper.ctx;
+  protected srcOpts = {
+    pattern: '**',
+    options: { dot: true },
+  };
+
+  get sFiles() {
+    const { pattern, options } = this.srcOpts;
+    return globby.sync(this.templatePath(pattern), options);
+  }
+
+  install() {
+    // ignore installing...
+  }
 }
